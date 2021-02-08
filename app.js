80e008f8024b40ab8cfd51332432d62d75abf2a3
sidratchy <sidrathchy17@gmail.com>
2021-02-07 17:31:13 -0800

    final

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 document.getElementById("search-Btn").addEventListener('click', function () {
-    const searchInput = document.getElementById('search-input').value.trim();
-    
-    
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
-      .then(res => res.json())
-      .then(data => displayMenu(data))
-  });
+  const searchInput = document.getElementById('search-input').value.trim();
+
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
+    .then(res => res.json())
+    .then(data => displayMenu(data))
+});
 
 
 
-  /// for display menu chart ////
-  const displayMenu = (menu) => {
-    const result = document.getElementById('menu-result');
-    if(document.getElementById('search-input').value == ""){
-      document.getElementById("required").innerHTML = 'input field required'
-    }else{
-      document.getElementById("required").style.display = 'none';
-if(menu.meals){
-  document.getElementById('search-input').value = "";
-  document.getElementById('notMatch').innerHTML = "";
+/// for display menu chart ////
+const displayMenu = (menu) => {
+  const result = document.getElementById('menu-result');
+  if (document.getElementById('search-input').value == "") {
+    document.getElementById("required").innerHTML = 'input field required'
+  } else {
+    document.getElementById("required").style.display = 'none';
+    if (menu.meals) {
+      document.getElementById('search-input').value = "";
+      document.getElementById('notMatch').innerHTML = "";
 
-  
-    menu.meals.forEach(meal => {
 
-      const myMeal = document.createElement("div");
-      const mealList = `
+      menu.meals.forEach(meal => {
+
+        const myMeal = document.createElement("div");
+        const mealList = `
         
      
       <div class="card shadow  h-100">
@@ -37,29 +37,29 @@ if(menu.meals){
         </div>
      `;
 
-      myMeal.innerHTML = mealList
-      
-      result.appendChild(myMeal);
-    
-    })
-  }else{
-   document.getElementById('notMatch').innerHTML = 'Do not Match your search.Please try again..';
-   document.getElementById('search-input').value = "";
+        myMeal.innerHTML = mealList
+
+        result.appendChild(myMeal);
+
+      })
+    } else {
+      document.getElementById('notMatch').innerHTML = 'Do not Match your search.Please try again..';
+      document.getElementById('search-input').value = "";
+    }
   }
-}
-  };
+};
 
-  //for recipeIngredients details.......
-const recipeIngredients = name =>{
-  
+//for recipeIngredients details.......
+const recipeIngredients = name => {
 
-      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
-      .then(res => res.json())
-      .then(data => {
-        const detail = data.meals[0];
-         
-        const menuRecipe = document.getElementById("details"); 
-        menuRecipe.innerHTML = `
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
+    .then(res => res.json())
+    .then(data => {
+      const detail = data.meals[0];
+
+      const menuRecipe = document.getElementById("details");
+      menuRecipe.innerHTML = `
         <div class="card mx-auto shadow" style="width:26rem;">
   <img src="${detail.strMealThumb}" class="card-img-top" alt="...">
   <div class="card-body">
@@ -83,7 +83,7 @@ const recipeIngredients = name =>{
          `
     });
 
-  }
+}
 
 
 
