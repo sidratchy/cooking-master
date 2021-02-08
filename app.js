d621ce780839536c7df824682d9ef0137a3df983
sidratchy <sidrathchy17@gmail.com>
2021-02-08 01:42:10 -0800

    this is final change

--- app.js
+++ app.js
@@ -11,24 +11,26 @@ document.getElementById("search-Btn").addEventListener('click', function () {
 
 /// for display menu chart ////
 const displayMenu = (menu) => {
-  const result = document.getElementById('menu-result');
+  const mealList = document.getElementById('menu-result');
+
   if (document.getElementById('search-input').value == "") {
     document.getElementById("required").innerHTML = 'input field required'
-  } else {
-    document.getElementById("required").style.display = 'none';
+  }
+
+  else {
+
+    document.getElementById("required").innerHTML = "";
+
     if (menu.meals) {
       document.getElementById('search-input').value = "";
       document.getElementById('notMatch').innerHTML = "";
-
+      mealList.innerHTML = "";
 
       menu.meals.forEach(meal => {
-
-        const myMeal = document.createElement("div");
-        const mealList = `
+        mealList.innerHTML += `
         
-     
-      <div class="card shadow  h-100">
-          <img src="${meal.strMealThumb}" class="card-img-top" alt="..." onclick="#recipeIngrideants(${meal.strMeal})">
+     <div class="card shadow  h-100">
+          <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
           <div class="card-body ">
             <h5 class="card-title text-center">${meal.strMeal}</h5>
             <button class="btn btn-link stretched-link" onclick="recipeIngredients('${meal.strMeal}')"></button>
@@ -37,14 +39,11 @@ const displayMenu = (menu) => {
         </div>
      `;
 
-        myMeal.innerHTML = mealList
-
-        result.appendChild(myMeal);
-
       })
     } else {
       document.getElementById('notMatch').innerHTML = 'Do not Match your search.Please try again..';
       document.getElementById('search-input').value = "";
+
     }
   }
 };
@@ -55,12 +54,15 @@ const recipeIngredients = name => {
 
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
     .then(res => res.json())
-    .then(data => {
-      const detail = data.meals[0];
+    .then(data => showRecipe(data));
 
-      const menuRecipe = document.getElementById("details");
-      menuRecipe.innerHTML = `
-        <div class="card mx-auto shadow" style="width:26rem;">
+
+  const showRecipe = mealName => {
+    const detail = mealName.meals[0];
+
+    const menuRecipe = document.getElementById("details");
+    menuRecipe.innerHTML = `
+        <div class="card mx-auto shadow w-75">
   <img src="${detail.strMealThumb}" class="card-img-top" alt="...">
   <div class="card-body">
     
@@ -81,7 +83,7 @@ const recipeIngredients = name => {
   </div>
 </div>
          `
-    });
+  }
 
 }
 
