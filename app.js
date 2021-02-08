2b3097049f1b7bde008d231be8c593a769c6cf6d
sidratchy <sidrathchy17@gmail.com>
2021-02-07 17:18:49 -0800

    change again

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 document.getElementById("search-Btn").addEventListener('click', function () {
     const searchInput = document.getElementById('search-input').value.trim();
+    
+    
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
       .then(res => res.json())
       .then(data => displayMenu(data))
@@ -9,33 +11,45 @@ document.getElementById("search-Btn").addEventListener('click', function () {
 
   /// for display menu chart ////
   const displayMenu = (menu) => {
+    const result = document.getElementById('menu-result');
+    if(document.getElementById('search-input').value == ""){
+      document.getElementById("required").innerHTML = 'input field required'
+    }else{
+      document.getElementById("required").style.display = 'none';
+if(menu.meals){
+  document.getElementById('search-input').value = "";
+  document.getElementById('notMatch').innerHTML = "";
 
+  
     menu.meals.forEach(meal => {
 
       const myMeal = document.createElement("div");
       const mealList = `
         
      
-      <div class="card shadow m-2 ssss">
+      <div class="card shadow  h-100">
           <img src="${meal.strMealThumb}" class="card-img-top" alt="..." onclick="#recipeIngrideants(${meal.strMeal})">
           <div class="card-body ">
             <h5 class="card-title text-center">${meal.strMeal}</h5>
-            <button class="btn btn-info text-white d-block mx-auto" onclick="recipeIngredients('${meal.strMeal}')">Details</button>
+            <button class="btn btn-link stretched-link" onclick="recipeIngredients('${meal.strMeal}')"></button>
            
           </div>
         </div>
-     
-    
-        `;
+     `;
+
       myMeal.innerHTML = mealList
-      const result = document.getElementById('menu-result');
+      
       result.appendChild(myMeal);
-
-    }
-
-    )
+    
+    })
+  }else{
+   document.getElementById('notMatch').innerHTML = 'Do not Match your search.Please try again..';
+   document.getElementById('search-input').value = "";
+  }
+}
   };
 
+  //for recipeIngredients details.......
 const recipeIngredients = name =>{
   
 
@@ -67,8 +81,7 @@ const recipeIngredients = name =>{
   </div>
 </div>
          `
-        
-      })
+    });
 
   }
 
